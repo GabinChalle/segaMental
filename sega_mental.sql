63fe0bbcbfb2ed1d608368d99346ab93484a7e23
Challe <gabin.challe@outlook.fr>
2019-10-23 15:48:16 +0200

    ajout du script de la base de données

---
+++ sega_mental.sql
@@ -0,0 +1,41 @@
+CREATE TABLE operations
+(
+    id_op INT PRIMARY KEY NOT NULL auto_increment,
+    score INT NOT NULL,
+    id_user INT NOT NULL,
+    CONSTRAINT fk_id_user
+    FOREIGN KEY (id_user)
+    REFERENCES USER(id_user)
+);
+
+CREATE TABLE utilisateur
+(
+    id_user INT PRIMARY KEY NOT NULL auto_increment,
+    pseudo VARCHAR(255),
+    password VARCHAR(255),
+    id_op INT,
+    CONSTRAINT fk_id_op
+    FOREIGN KEY (id_op)
+    REFERENCES operations(id_op)
+);
+
+CREATE TABLE expressions
+(
+	id_calcul INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	libelle VARCHAR(255),
+	res_attendu FLOAT,
+	res_donnee FLOAT,
+	id_op INT,
+	CONSTRAINT fk_id_ex
+	FOREIGN KEY (id_op)
+	REFERENCES operations(id_op)
+);
+
+INSERT INTO utilisateur (pseudo, PASSWORD)
+ VALUES
+ ('gabin', 'challe'),
+ ('corentin', 'gautier'),
+ ('yves', 'guerin');
+ 
+ 
+
